15a9b4d576e82edf3efefc77ab395d5badfdd7c2
Wei Wei <lyweiwei@outlook.com>
2016-07-15 10:49:56 +0800

    Refactor and add more test cases
    1. Test the element viewport
    2. Test the variant height cases
    3. Fix the scroll item to middle inaccurate bug

--- demos/variant-height/index.js
+++ demos/variant-height/index.js
@@ -4,7 +4,9 @@ import ListView from '../../js/index';
 import 'style!css!./index.css';
 
 const listView = window.listView = new ListView({
-  items: _.map(_.range(20000), i => ({ text: `${i}: ${_.map(_.range(_.random(50)), () => _.random(9)).join('')}` })),
+  items: _.map(_.range(20000), i => ({
+    text: `${i}: ${_.map(_.range(_.random(50)), () => _.random(9)).join('')}`,
+  })),
   viewport: '.container',
 }).render();
 $('.container').append(listView.$el);
